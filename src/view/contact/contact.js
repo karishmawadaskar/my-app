d2570c4dedd497ae60f115dd907ddff4e5ff2ab3
karishma wadaskar <karishmawadaskar@gmail.com>
2024-08-21 16:42:33 +0530

    added contact page

--- src/view/contact/contact.js
+++ src/view/contact/contact.js
@@ -3,10 +3,10 @@ import Navbar from "../../componant/navbar/navbar"
 
 
 function Contact() {
-  return (<div><Navbar/>
+  return (<div><Navbar />
     <div className='heading'><h1>Contact</h1></div>
-    <img src='https://img.freepik.com/free-vector/content-filling-concept-making-responsive-viral-content-business-development-idea-digital-strategy-business-promotion-communication-with-customer-flat-vector-illustration_613284-1811.jpg?t=st=1724237552~exp=1724241152~hmac=e918a811349571dc53c3f78939d12057e94e572f803efa36335adade479ff9d7&w=996'className='navbar-image'/>
-    </div>
+    <img src='https://img.freepik.com/free-vector/content-filling-concept-making-responsive-viral-content-business-development-idea-digital-strategy-business-promotion-communication-with-customer-flat-vector-illustration_613284-1811.jpg?t=st=1724237552~exp=1724241152~hmac=e918a811349571dc53c3f78939d12057e94e572f803efa36335adade479ff9d7&w=996' className='navbar-image' />
+  </div>
   )
 }
 
