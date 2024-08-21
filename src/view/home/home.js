30dd432993b523fe4f6bafb8bdaf79dec93c6d92
karishma wadaskar <karishmawadaskar@gmail.com>
2024-08-21 16:41:55 +0530

    added home page

--- src/view/home/home.js
+++ src/view/home/home.js
@@ -3,10 +3,10 @@ import Navbar from "../../componant/navbar/navbar"
 import "./../../index.css";
 
 function home() {
-  return (<div><Navbar/>
+  return (<div><Navbar />
     <div className='heading' ><h1>Home</h1></div>
- <img src='https://img.freepik.com/free-vector/beautiful-home_24877-50819.jpg?t=st=1724237135~exp=1724240735~hmac=85f1a96570819d9534bfe92fd73d753e1c8e01dd8e94fab2e6ed0c841450da36&w=740' className='navbar-image'/>
-    </div>
+    <img src='https://img.freepik.com/free-vector/beautiful-home_24877-50819.jpg?t=st=1724237135~exp=1724240735~hmac=85f1a96570819d9534bfe92fd73d753e1c8e01dd8e94fab2e6ed0c841450da36&w=740' className='navbar-image' />
+  </div>
   )
 }
 
